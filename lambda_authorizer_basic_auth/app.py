7987c45fa79d5b07b990fd538b2fb086ffdf00c7
Phillip Spies <pspies@amazon.com>
2023-02-08 14:11:18 -0500

    master

--- lambda_authorizer_basic_auth/app.py
+++ lambda_authorizer_basic_auth/app.py
@@ -1,10 +1,3 @@
-"""
-   Copyright 2018 Amazon.com, Inc. and its affiliates. All Rights Reserved.
-
-   Licensed under the MIT License. See the LICENSE accompanying this file
-   for the specific language governing permissions and limitations under
-   the License.
-"""
 from __future__ import print_function
 
 import os
